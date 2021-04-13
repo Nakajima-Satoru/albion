7286a464e38cdd59026e7e59b997d69907f1d627
nakatsuji masato <nakatsuji@teastalk.jp>
2021-04-13 18:57:46 +0900

    4/13

--- core/Core.js
+++ core/Core.js
@@ -2,6 +2,17 @@ module.exports=class Core{
 
     constructor(ro){
         this.ro=ro;
+
+        this.model={
+            load:function(modelNames){
+
+            },
+            get:function(modelName){
+
+            },
+        };
     }
+
+
 
 }
