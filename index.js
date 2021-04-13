cb9bb361eaf591d53dc4194a971b3b1212db7291
nakatsuji masato <nakatsuji@teastalk.jp>
2021-04-13 15:43:51 +0900

    4/13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports={
-    fs:require("alvion/core/fs.js"),
-    sync:require("alvion/core/sync.js"),
-    http:require("alvion/core/http.js"),
-    Controller:require("alvion/core/Controller.js"),
+    fs:require("./core/fs.js"),
+    sync:require("./core/sync.js"),
+    http:require("./core/http.js"),
+    text:require("./core/text.js"),
+    Controller:require("./core/Controller.js"),
+    Model:require("./core/Model.js"),
 };
